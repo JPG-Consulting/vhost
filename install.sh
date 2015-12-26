836af3624adfc74b18398ceda6c6a3405d3acc3c
jpg-consulting <opensource@jpg-consulting.es>
2015-12-26 13:21:50 +0100

    updates

--- install.sh
+++ install.sh
@@ -56,6 +56,14 @@ VIRTUALMAIL_GROUP_ID=5000
 VIRTUALMAIL_MBOXES_PATH='/var/vmail'
 
 # ------------------------------------------------------------------
+#  FTP
+# ------------------------------------------------------------------
+FTP_GROUP_NAME='ftpgroup'
+FTP_GROUP_ID=2001
+FTP_USER_NAME='ftpuser'
+FTP_USER_ID=2001
+
+# ------------------------------------------------------------------
 #  Non Privileged user
 # ------------------------------------------------------------------
 if prompt_yn "Do you wish to add a non-privileged user?"; then
@@ -384,10 +392,52 @@ mysql -uroot -p$MYSQL_ROOT_PASSWORD <<EOF
         FOREIGN KEY (mail_id) REFERENCES mail(id) ON DELETE CASCADE
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci;
 
+    CREATE TABLE IF NOT EXISTS sys_groups (
+	    gid smallint(6) NOT NULL DEFAULT '$FTP_GROUP_ID',
+        groupname varchar(16) NOT NULL,
+        members varchar(16) NOT NULL,
+        KEY groupname (groupname)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+    CREATE TABLE IF NOT EXISTS sys_users (
+        id int(10) unsigned NOT NULL AUTO_INCREMENT,
+        userid varchar(32) COLLATE utf8_general_ci NOT NULL DEFAULT '',
+        passwd varchar(32) COLLATE utf8_general_ci NOT NULL DEFAULT '',
+        uid smallint(6) NOT NULL DEFAULT '$FTP_USER_ID',
+        gid smallint(6) NOT NULL DEFAULT '$FTP_GROUP_ID',
+        homedir varchar(255) COLLATE utf8_general_ci NOT NULL DEFAULT '',
+        shell varchar(16) COLLATE utf8_general_ci NOT NULL DEFAULT '/sbin/nologin',
+        PRIMARY KEY (id),
+        UNIQUE KEY userid (userid)
+    ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+    INSERT INTO domains (name) VALUES ('$HOSTNAME');
+	
 	GRANT ALL PRIVILEGES ON $MYSQL_CONTROLPANEL_DATABASE.* TO $MYSQL_CONTROLPANEL_USER_NAME@localhost;
 
     FLUSH PRIVILEGES;
 EOF
+
+if [ -n "$USER_NAME" ]; then
+    USER_GID=$(id -u $USER_NAME)
+    if [ $? -eq 0 ]; then
+        mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_CONTROLPANEL_DATABASE -e "INSERT INTO sys_groups (ugid, groupname, members) VALUES ('$USER_GID', '$USER_GID', NULL);"
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to add $USER_NAME to sys_groups table."
+            exit 1
+        fi
+
+        USER_ID=$(id -u $USER_NAME)
+        if [ $? -eq 0 ]; then
+            SYS_USERS_PASSWORD=$(echo "{md5}"`/bin/echo -n "$USER_PASSWORD" | openssl dgst -binary -md5 | openssl enc -base64`)
+            mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_CONTROLPANEL_DATABASE -e "INSERT INTO sys_users (userid, passwd, uid, gid, homedir, shell) VALUES ('$USER_NAME', '$SYS_USERS_PASSWORD', '$USER_ID', '$USER_GID', '/home/$USER_NAME', '/bin/bash');"
+            if [ $? -ne 0 ]; then
+                echo "Error: Failed to add $USER_NAME to sys_users table."
+                exit 1
+            fi
+        fi
+    fi
+fi
 
 # ==================================================================
 #  PHP
@@ -631,9 +681,190 @@ if [ ! -f /etc/apache2/sites-available/$HOSTNAME ]; then
 fi
 
 # ------------------------------------------------------------------
-#  Restart the apache2 service
+#  Create the control panel vhost
 # ------------------------------------------------------------------
-service apache2 restart
+if [ ! -d /usr/share/phpanel/public ]; then
+    mkdir -p /usr/share/phpanel/public
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to create /usr/share/phpanel."
+        exit 1
+    fi
+
+    chmod -R +755 /usr/share/phpanel
+fi
+
+if [ ! -f /etc/apache2/conf.d/phpanel.conf ]; then
+    cat <<EOF > /etc/apache2/conf.d/phpanel.conf
+<IfModule mod_ssl.c>
+    Listen 8443
+    <VirtualHost _default_:8443>
+        ServerAdmin webmaster@localhost
+
+        DocumentRoot /usr/share/phpanel/public
+        <Directory />
+                Options FollowSymLinks
+                AllowOverride None
+        </Directory>
+        <Directory /var/www/jpg-consulting.es/htsdocs/>
+                Options Indexes FollowSymLinks MultiViews
+                AllowOverride None
+                Order allow,deny
+                allow from all
+        </Directory>
+
+        ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/
+        <Directory "/usr/lib/cgi-bin">
+                AllowOverride None
+                Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
+                Order allow,deny
+                Allow from all
+        </Directory>
+
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+
+        # Possible values include: debug, info, notice, warn, error, crit,
+        # alert, emerg.
+        LogLevel warn
+
+        CustomLog ${APACHE_LOG_DIR}/ssl_access.log combined
+
+        #   SSL Engine Switch:
+        #   Enable/Disable SSL for this virtual host.
+        SSLEngine on
+
+        #   A self-signed (snakeoil) certificate can be created by installing
+        #   the ssl-cert package. See
+        #   /usr/share/doc/apache2.2-common/README.Debian.gz for more info.
+        #   If both key and certificate are stored in the same file, only the
+        #   SSLCertificateFile directive is needed.
+        SSLCertificateFile    /etc/ssl/certs/ssl-cert-snakeoil.pem
+        SSLCertificateKeyFile /etc/ssl/private/ssl-cert-snakeoil.key
+
+        #   Server Certificate Chain:
+        #   Point SSLCertificateChainFile at a file containing the
+        #   concatenation of PEM encoded CA certificates which form the
+        #   certificate chain for the server certificate. Alternatively
+        #   the referenced file can be the same as SSLCertificateFile
+        #   when the CA certificates are directly appended to the server
+        #   certificate for convinience.
+        #SSLCertificateChainFile /etc/apache2/ssl.crt/server-ca.crt
+
+        #   Certificate Authority (CA):
+        #   Set the CA certificate verification path where to find CA
+        #   certificates for client authentication or alternatively one
+        #   huge file containing all of them (file must be PEM encoded)
+        #   Note: Inside SSLCACertificatePath you need hash symlinks
+        #         to point to the certificate files. Use the provided
+        #         Makefile to update the hash symlinks after changes.
+        #SSLCACertificatePath /etc/ssl/certs/
+        #SSLCACertificateFile /etc/apache2/ssl.crt/ca-bundle.crt
+
+        #   Certificate Revocation Lists (CRL):
+        #   Set the CA revocation path where to find CA CRLs for client
+        #   authentication or alternatively one huge file containing all
+        #   of them (file must be PEM encoded)
+        #   Note: Inside SSLCARevocationPath you need hash symlinks
+        #         to point to the certificate files. Use the provided
+        #         Makefile to update the hash symlinks after changes.
+        #SSLCARevocationPath /etc/apache2/ssl.crl/
+        #SSLCARevocationFile /etc/apache2/ssl.crl/ca-bundle.crl
+
+        #   Client Authentication (Type):
+        #   Client certificate verification type and depth.  Types are
+        #   none, optional, require and optional_no_ca.  Depth is a
+        #   number which specifies how deeply to verify the certificate
+        #   issuer chain before deciding the certificate is not valid.
+        #SSLVerifyClient require
+        #SSLVerifyDepth  10
+
+        #   Access Control:
+        #   With SSLRequire you can do per-directory access control based
+        #   on arbitrary complex boolean expressions containing server
+        #   variable checks and other lookup directives.  The syntax is a
+        #   mixture between C and Perl.  See the mod_ssl documentation
+        #   for more details.
+        #<Location />
+        #SSLRequire (    %{SSL_CIPHER} !~ m/^(EXP|NULL)/ \
+        #            and %{SSL_CLIENT_S_DN_O} eq "Snake Oil, Ltd." \
+        #            and %{SSL_CLIENT_S_DN_OU} in {"Staff", "CA", "Dev"} \
+        #            and %{TIME_WDAY} >= 1 and %{TIME_WDAY} <= 5 \
+        #            and %{TIME_HOUR} >= 8 and %{TIME_HOUR} <= 20       ) \
+        #           or %{REMOTE_ADDR} =~ m/^192\.76\.162\.[0-9]+$/
+        #</Location>
+
+        #   SSL Engine Options:
+        #   Set various options for the SSL engine.
+        #   o FakeBasicAuth:
+        #     Translate the client X.509 into a Basic Authorisation.  This means that
+        #     the standard Auth/DBMAuth methods can be used for access control.  The
+        #     user name is the `one line' version of the client's X.509 certificate.
+        #     Note that no password is obtained from the user. Every entry in the user
+        #     file needs this password: `xxj31ZMTZzkVA'.
+        #   o ExportCertData:
+        #     This exports two additional environment variables: SSL_CLIENT_CERT and
+        #     SSL_SERVER_CERT. These contain the PEM-encoded certificates of the
+        #     server (always existing) and the client (only existing when client
+        #     authentication is used). This can be used to import the certificates
+        #     into CGI scripts.
+        #   o StdEnvVars:
+        #     This exports the standard SSL/TLS related `SSL_*' environment variables.
+        #     Per default this exportation is switched off for performance reasons,
+        #     because the extraction step is an expensive operation and is usually
+        #     useless for serving static content. So one usually enables the
+        #     exportation for CGI and SSI requests only.
+        #   o StrictRequire:
+        #     This denies access when "SSLRequireSSL" or "SSLRequire" applied even
+        #     under a "Satisfy any" situation, i.e. when it applies access is denied
+        #     and no other module can change it.
+        #   o OptRenegotiate:
+        #     This enables optimized SSL connection renegotiation handling when SSL
+        #     directives are used in per-directory context.
+        #SSLOptions +FakeBasicAuth +ExportCertData +StrictRequire
+        <FilesMatch "\.(cgi|shtml|phtml|php)$">
+                SSLOptions +StdEnvVars
+        </FilesMatch>
+        <Directory /usr/lib/cgi-bin>
+                SSLOptions +StdEnvVars
+        </Directory>
+
+        #   SSL Protocol Adjustments:
+        #   The safe and default but still SSL/TLS standard compliant shutdown
+        #   approach is that mod_ssl sends the close notify alert but doesn't wait for
+        #   the close notify alert from client. When you need a different shutdown
+        #   approach you can use one of the following variables:
+        #   o ssl-unclean-shutdown:
+        #     This forces an unclean shutdown when the connection is closed, i.e. no
+        #     SSL close notify alert is send or allowed to received.  This violates
+        #     the SSL/TLS standard but is needed for some brain-dead browsers. Use
+        #     this when you receive I/O errors because of the standard approach where
+        #     mod_ssl sends the close notify alert.
+        #   o ssl-accurate-shutdown:
+        #     This forces an accurate shutdown when the connection is closed, i.e. a
+        #     SSL close notify alert is send and mod_ssl waits for the close notify
+        #     alert of the client. This is 100% SSL/TLS standard compliant, but in
+        #     practice often causes hanging connections with brain-dead browsers. Use
+        #     this only for browsers where you know that their SSL implementation
+        #     works correctly.
+        #   Notice: Most problems of broken clients are also related to the HTTP
+        #   keep-alive facility, so you usually additionally want to disable
+        #   keep-alive for those clients, too. Use variable "nokeepalive" for this.
+        #   Similarly, one has to force some clients to use HTTP/1.0 to workaround
+        #   their broken HTTP/1.1 implementation. Use variables "downgrade-1.0" and
+        #   "force-response-1.0" for this.
+        BrowserMatch "MSIE [2-6]" \
+                nokeepalive ssl-unclean-shutdown \
+                downgrade-1.0 force-response-1.0
+        # MSIE 7 and newer should be able to use keepalive
+        BrowserMatch "MSIE [17-9]" ssl-unclean-shutdown
+
+        # Include phpMyAdmin configuration:
+        Include /etc/phpmyadmin/apache.conf
+
+    </VirtualHost>
+</IfModule>
+
+EOF
+fi
 
 # ==================================================================
 #  PHPMyAdmin
@@ -655,6 +886,20 @@ if [ $INSTALL_PHPMYADMIN -eq 0 ]; then
         exit 1
     fi
 fi
+
+if [ ! -f /etc/phpmyadmin/apache.conf ]; then
+    if [ -f /etc/apache2/conf.d/phpmyadmin.conf ]; then
+        cp /etc/apache2/conf.d/phpmyadmin.conf /etc/phpmyadmin/apache.conf
+    fi
+fi
+if [ -f /etc/apache2/conf.d/phpmyadmin.conf ]; then
+    rm /etc/apache2/conf.d/phpmyadmin.conf
+fi
+
+# ==================================================================
+#  Restart the apache2 service
+# ==================================================================
+service apache2 restart
 
 # ==================================================================
 #  AWStats
@@ -699,6 +944,74 @@ if [ $INSTALL_PROFTPD -eq 0 ]; then
             exit 1
         fi
     fi
+
+    if ! getent group $FTP_GROUP_NAME>/dev/null; then
+        addgroup --system --gid $FTP_GROUP_ID $FTP_GROUP_NAME
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to add group $FTP_GROUP_NAME."
+            exit 1
+        fi
+    fi
+
+    if ! getent passwd $FTP_USER_NAME>/dev/null; then
+        adduser --system --home /var/www --uid $FTP_UUSER_ID --ingroup $FTP_GROUP_NAME --disabled-login $FTP_USER_NAME
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to add user $FTP_USER_NAME."
+            exit 1
+        fi
+    fi
+
+    sed -i 's|# RequireValidShell|RequireValidShell|g' /etc/proftpd/proftpd.conf
+    sed -i 's|# DefaultRoot|DefaultRoot|g' /etc/proftpd/proftpd.conf
+	sed -i 's|#Include /etc/proftpd/sql.conf|Include /etc/proftpd/sql.conf|g' /etc/proftpd/proftpd.conf
+
+    cat << EOF > /etc/proftpd/sql.conf
+#
+# Proftpd sample configuration for SQL-based authentication.
+#
+# (This is not to be used if you prefer a PAM-based SQL authentication)
+#
+
+<IfModule mod_sql.c>
+#
+# Choose a SQL backend among MySQL or PostgreSQL.
+# Both modules are loaded in default configuration, so you have to specify the backend
+# or comment out the unused module in /etc/proftpd/modules.conf.
+# Use 'mysql' or 'postgres' as possible values.
+#
+SQLBackend     mysql
+
+#SQLEngine on
+#SQLAuthenticate on
+SQLAuthenticate users groups
+#
+# Use both a crypted or plaintext password
+SQLAuthTypes OpenSSL Crypt
+
+#
+# Connection
+SQLConnectInfo $MYSQL_CONTROLPANEL_DATABASE@localhost $MYSQL_CONTROLPANEL_USER_NAME $MYSQL_CONTROLPANEL_USER_PASSWORD
+
+# Describes both users/groups tables
+#
+SQLUserInfo sys_users userid passwd uid gid homedir shell
+SQLGroupInfo sys_groups groupname gid members
+
+# set min UID and GID - otherwise these are 999 each
+SQLMinID        500
+
+RootLogin off
+RequireValidShell off
+
+</IfModule>
+EOF
+
+    sed -i 's|#LoadModule mod_sql.c|LoadModule mod_sql.c|g' /etc/proftpd/modules.conf
+    sed -i 's|#LoadModule mod_sql_mysql.c|LoadModule mod_sql_mysql.c|g' /etc/proftpd/modules.conf
+
+    mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_CONTROLPANEL_DATABASE -e "INSERT INTO sys_groups (groupname, gid, members) VALUES ('$FTP_GROUP_NAME', '$FTP_GROUP_ID', NULL);"
+
+	service proftpd restart
 fi
 
 # ==================================================================
